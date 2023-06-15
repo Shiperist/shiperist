a97a764a33312fd4655d3ef83ae388dd9969743e
Zunw <43964753+ZunwDev@users.noreply.github.com>
2023-06-15 23:34:37 +0200

    enhancement(ui) changed how readonly button looks, and some small changes

--- dashboard/src/components/base/button.tsx
+++ dashboard/src/components/base/button.tsx
@@ -8,10 +8,10 @@ export const Button = ({
   className?: string;
   disabled?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  const buttonClasses = `rounded-md border-transparent bg-amber-200 px-4 py-2 text-sm font-medium text-amber-900 ${
+  const buttonClasses = `rounded-md border-transparent px-4 py-2 text-sm font-medium ${
     !disabled
-      ? 'hover:bg-amber-400 hover:border-transparent cursor-pointer'
-      : '!cursor-default opacity-80'
+      ? 'hover:bg-amber-400 bg-amber-200 text-amber-900 hover:border-transparent cursor-pointer'
+      : '!cursor-default bg-gray-200 text-gray-400 opacity-80'
   } transition duration-150 ease-in-out ${className}`;
 
   return <button className={buttonClasses.trim()} {...props}></button>;
