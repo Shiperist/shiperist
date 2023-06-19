db49b73c024cf3d9ea3f0f0914d5218b421cb177
zZHorizonZz <danfiala23@gmail.com>
2023-06-19 14:31:54 +0200

    enhancement(apps): minor ux enhancements of app list

--- dashboard/src/components/app/app-list.tsx
+++ dashboard/src/components/app/app-list.tsx
@@ -28,56 +28,8 @@ const AppList = () => {
     void router.push(`/app/${id}`);
   };
 
-  /*
-  <Card className="mt-4 overflow-auto p-0 cursor-pointer drop-shadow-s border-0 bg-white ring-0">
-    //TODO Scaleaway style
-    <List>
-      {apps?.map((app) => {
-        const icon = Types[app.os]?.icon;
-        const trustedIcon: { __html: string | TrustedHTML } | undefined = icon
-          ? { __html: icon as string | TrustedHTML }
-          : undefined;
-        return (
-          <ListItem
-            key={app.id}
-            className="hover:bg-gray-100 p-5 border-none"
-            onClick={() => navigateToApp(app.id)}
-          >
-            <img
-              className="w-12 h-12 rounded-full mr-4"
-              alt="preview-img"
-              src="https://images.unsplash.com/photo-1686216941182-0f5699f4584d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=725&q=80"
-            />
-            <div className="flex flex-col flex-1">
-              <text className="font-bold text-gray-800">{app.name}</text>
-              <text className="text-sm text-gray-500">{app.description}</text>
-            </div>
-            <div className="flex flex-col flex-1">
-              <text className="font-bold text-gray-800">ID</text>
-              <text className="text-sm text-gray-500">{app.id}</text>
-            </div>
-            <div className="flex flex-1 flex-row">
-              <div
-                className="border-2 mr-4 w-12 h-12 p-3 rounded-full place-content-center text-2xl"
-                style={{ borderColor: Types[app.os]?.color }}
-                dangerouslySetInnerHTML={trustedIcon}
-              />
-              <Badge size="xs" color="green" className="self-center">
-                {app.platform}
-              </Badge>
-            </div>
-            <text className="text-gray-500 mt-auto mb-auto">
-              Updated 5m ago
-            </text>
-          </ListItem>
-        );
-      })}
-    </List>
-  </Card>
-   */
-
   return (
-    <Grid className="gap-4 grid-cols-3 grid-rows-3 mt-8 px-8 mt-8 mx-auto max-w-7xl">
+    <Grid className="gap-4 grid-cols-3 grid-rows-3 px-8 mt-8 mx-auto max-w-7xl">
       {apps?.map((app) => {
         const icon = Types[app.os]?.icon;
         const trustedIcon: { __html: string | TrustedHTML } | undefined = icon
