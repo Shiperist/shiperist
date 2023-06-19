ea588e26a4840351cebe6b87bfedfc324581b676
Zunw <43964753+ZunwDev@users.noreply.github.com>
2023-06-19 21:13:13 +0200

    enhancement(global) fixed white bar at bottom of profile page, and few improvements

---
+++ dashboard/src/utils/scroll.ts
@@ -0,0 +1,13 @@
+export const scrollToPos = (elementPos: number) => {
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    });
+  };
+
+export const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
